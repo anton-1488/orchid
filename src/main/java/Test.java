f6d2eca5dcfa490e1aa784e60c410d1cf513a66b
anton-1488 <app67253100@gmail.com>
2026-04-07 23:49:59 +0300

    First commit of my orchid fork

---
+++ src/main/java/Test.java
@@ -0,0 +1,31 @@
+import com.subgraph.orchid.TorClient;
+import com.subgraph.orchid.TorInitializationListener;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Socket;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        TorClient client = new TorClient();
+        client.addInitializationListener(new TorInitializationListener() {
+            public void initializationProgress(String message, int percent) {
+                System.out.println("Загрузка: " + percent + "% - " + message);
+            }
+            public void initializationCompleted() {
+                System.out.println("Tor готов!");
+            }
+        });
+
+        client.start();
+        client.waitUntilReady();
+
+        Proxy proxy = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", 9050));
+
+        Socket socket = new Socket(proxy);
+        socket.connect(InetSocketAddress.createUnresolved("check.torproject.org", 80));
+        System.out.println("Победа! Мы в сети Tor.");
+        socket.close();
+        client.stop();
+    }
+}
